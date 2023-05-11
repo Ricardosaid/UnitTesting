449e62204873bc2d78370866738abc5439576db8
Ricardo Said <ricardo@smartpaymx.com>
2023-05-11 00:15:40 -0600

    added mock and methods

--- UnitTestingApi/Services/PersonService.cs
+++ UnitTestingApi/Services/PersonService.cs
@@ -5,13 +5,26 @@ namespace UnitTestingApi.Services;
 
 public class PersonService : IPersonService
 {
-    public IEnumerable<Person> Get()
-    {
-        throw new NotImplementedException();
-    }
 
-    public Person? Get(int id)
+    private List<Person> _persons = new()
     {
-        throw new NotImplementedException();
-    }
+        new Person
+        {
+            id = 1,
+            nombre = "NombreOne",
+            apellido = "apellidoOne",
+            edad = 20
+        },
+        new Person
+        {
+            id = 2,
+            nombre = "NombreDos",
+            apellido = "apellidoDos",
+            edad = 21
+        }
+    };
+
+    public IEnumerable<Person> Get() => _persons;
+
+    public Person? Get(int id) => _persons.FirstOrDefault(d => d.id == id);
 }
